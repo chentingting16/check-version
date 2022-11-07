cb3a90662510204ea316bd5ff8994ebbcfaa6b49
chentingting16 <42869208+chentingting16@users.noreply.github.com>
2022-11-07 09:12:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -87,37 +87,15 @@ for (i = 0; i < s.length; i++) {
 }
 
 var json_data = JSON.stringify(action_list);
-console.log(json_data);
 
-let mysqlExec = require('./util.js');
+
 
 var actions_db = [];
-async function getExistAction() {
-    var  sql = 'SELECT actions FROM action where project = ? and workflow = ?';
-    let params =[event.repository.id, process.env.GITHUB_WORKFLOW];
-    let [error, data] = await mysqlExec(sql, params);
-    if (error) {
-        if (data == null || data[0] == null) return true;
-        let actions_obj = JSON.parse(data[0].actions);
-        //[{\"name\":\"actions/checkout\",\"version\":\"v2\"},{\"name\":\"actions/cache\",\"version\":\"v2\"},{\"name\":\"actions/stale\",\"version\":\"v6.0.1\"}]
-        let i = 0;
-        for (let obj of actions_obj) {
-            console.log(`name:${obj.name}`);
-            actions_db[i] = new Action(obj.name,obj.version);
-            i++;
-        }
-        return false;
-    } else {
-        console.log('sql执行失败'+data);
-    }
-    return true;
-}
-
+let mysqlExec = require('./util.js');
 getExistAction().then((res)=>{
     if (res) {
         console.log("数据库中无该配置文件，新增");
         //查询当前 模糊版本的确切版本
-        
         insertAction(json_data);
     } else {
         console.log("有数据了");
@@ -131,6 +109,8 @@ getExistAction().then((res)=>{
 },(res)=>{ console.log("运行错误1:"+res);
 });
 
+
+
 async function insertAction(json_data) {
     let sql = "INSERT INTO action(project,workflow,actions,last_modified) VALUES (?,?,?,now())";
     let params=[event.repository.id, process.env.GITHUB_WORKFLOW, json_data];
@@ -141,10 +121,6 @@ async function insertAction(json_data) {
         console.log('sql执行失败'+data);
     }
 }
-
-
-
-
 
 async function getVersion(owner, repo) {
     //console.log(owner+" "+repo);
@@ -161,72 +137,3 @@ async function getVersion(owner, repo) {
     return response.data;
 }
 
-
-// function getV(owner, repo) {
-//     getVersion(owner, repo).then((v)=>{
-//         //console.log('v.data:  ', v.data);
-//         for (let obj of v.data) {
-//             console.log(`name:${obj.name}` + ` version:${obj.tag_name}`);
-//         }
-//     },(v)=>{ console.log("运行错误2:"+ JSON.stringify(res)); });
-// }
-
-
-
-
-
-
-//dajfklja
-// let connection =mysql.createConnection({
-//     host: "rm-uf60x57re73u05414go.mysql.rds.aliyuncs.com",//连接本地计算机
-//     port:3306,//端口
-//     user:"ctt",//数据库账号
-//     password:"Hello123",//密码
-//     database:"action"//连接的数据库名
-// });
-
-// //调用connect方法创造连接
-// connection.connect((err)=>{//回调函数,如果报错会把err填充上
-//     if(err){
-//         console.error("连接失败"+err.stack);//打印堆栈信息
-//         return;
-//     }
-//     console.log("连接成功");
-// });
-
-// var  sql2 = 'SELECT actions FROM action where project = ? and workflow = ?';
-// let params2=[event.repository.id, process.env.GITHUB_WORKFLOW];
-// //查
-// connection.query(sql2, params2, (err2,result2)=>{
-//     if(err2){
-//         console.log('[SELECT ERROR] - ',err2.message);
-//         return;
-//     }
-//     if (JSON.stringify(result2) != '{}' && JSON.stringify(result2) != '[]') {
-//         console.log("已有配置文件"+typeof(result2));
-//         console.log(JSON.stringify(result2));
-//         console.log(result2[1].toString());
-//         console.log(result2[2].toString());
-//         //配置文件与数据库中对比
-//         //connection.end();
-//         process.exit(0);
-//     }
-// });
-
-// let sql = "INSERT INTO action(project,workflow,actions,last_modified) VALUES (?,?,?,now())";
-// //设置参数
-// let params=[event.repository.id, process.env.GITHUB_WORKFLOW, json_data];
-// connection.query(sql,params,(err,result)=>{
-//     if (err) {
-//          console.error("插入失败" + err.message);
-//          return;
-//     }
-//     console.log("新配置文件，插入成功");
-// });
-
-// //关闭数据库连接
-// connection.end();
-
-// // console.log(event.repository.url);
-// // console.log(event);
-// // console.log(process.argv);
